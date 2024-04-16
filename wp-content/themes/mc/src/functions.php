bd379f37ab4141ce064439a654a4fd1229e73d70
Adachi Masakazu <marscode48@gmail.com>
2024-04-16 16:16:47 +0900

    アーカイブページを有効化

--- wp-content/themes/mc/src/functions.php
+++ wp-content/themes/mc/src/functions.php
@@ -85,3 +85,16 @@ function add_module($tag, $handle) {
 }
 
 add_filter('script_loader_tag', 'add_module', 10, 2);
+
+
+/**************************************************
+投稿のアーカイブページを作成
+**************************************************/
+function post_has_archive($args, $post_type) {
+  if ('post' == $post_type) {
+    $args['rewrite'] = true; // リライトを有効にする
+    $args['has_archive'] = 'works'; // 任意のスラッグ名
+  }
+  return $args;
+}
+add_filter('register_post_type_args', 'post_has_archive', 10, 2);
