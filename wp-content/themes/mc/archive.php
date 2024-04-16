18b1f0fa9b7bc74ae1a74231e5e4b9e3d71bd45c
Adachi Masakazu <marscode48@gmail.com>
2024-04-16 20:54:04 +0900

    archive.php/works.htmlからコピー

---
+++ wp-content/themes/mc/archive.php
@@ -0,0 +1,159 @@
+<!-- content -->
+<main class="content">
+  <canvas id="mainCanvas"></canvas>
+
+  <!-- works -->
+  <section id="works" class="works-page section">
+    <div class="container">
+      <div class="section__heading">
+        <h2 class="heading-primary tween-animate-title">WORKS</h2>
+      </div>
+      <div class="section__body appear up">
+        <div class="works-page__list item">
+          <!-- swiper-main -->
+          <div class="swiper swiper-main">
+            <div class="swiper-wrapper">
+              <div class="swiper-slide">
+                <div class="slide">
+                  <div class="slide__img-wrapper"><img class="slide__img" src="images/works-image1.jpg" alt="worksスライド画像一覧"></div>
+                  <p class="slide__title">MarsDesign</p>
+                  <p class="slide__text">WordPress / Swiper</p>
+                  <div class="slide__link">
+                    <a href="article-01.html" class="btn">more
+                      <i class="btn-icon fa-solid fa-arrow-right"></i>
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div class="swiper-slide">
+                <div class="slide">
+                  <div class="slide__img-wrapper"><img class="slide__img" src="images/works-image2.jpg" alt="worksスライド画像一覧"></div>
+                  <p class="slide__title">MarsCafe</p>
+                  <p class="slide__text">アニメーションサイト / GSAP</p>
+                  <div class="slide__link">
+                    <a href="article-01.html" class="btn">more
+                      <i class="btn-icon fa-solid fa-arrow-right"></i>
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div class="swiper-slide">
+                <div class="slide">
+                  <div class="slide__img-wrapper"><img class="slide__img" src="images/works-image3.jpg" alt="worksスライド画像一覧"></div>
+                  <p class="slide__title">MarsCode</p>
+                  <p class="slide__text">3Dサイト / WebGL(Three.js)</p>
+                  <div class="slide__link">
+                    <a href="article-01.html" class="btn">more
+                      <i class="btn-icon fa-solid fa-arrow-right"></i>
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div class="swiper-slide">
+                <div class="slide">
+                  <div class="slide__img-wrapper"><img class="slide__img" src="images/works-image4.jpg" alt="worksスライド画像一覧"></div>
+                  <p class="slide__title">リベルテ</p>
+                  <p class="slide__text">ECサイト / Shopify</p>
+                  <div class="slide__link">
+                    <a href="article-01.html" class="btn">more
+                      <i class="btn-icon fa-solid fa-arrow-right"></i>
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div class="swiper-slide">
+                <div class="slide">
+                  <div class="slide__img-wrapper"><img class="slide__img" src="images/works-image5.jpg" alt="worksスライド画像一覧"></div>
+                  <p class="slide__title">Mars Vintage</p>
+                  <p class="slide__text">Webアプリケーション(Vue.js) / SPA</p>
+                  <div class="slide__link">
+                    <a href="article-01.html" class="btn">more
+                      <i class="btn-icon fa-solid fa-arrow-right"></i>
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div class="swiper-slide">
+                <div class="slide">
+                  <div class="slide__img-wrapper"><img class="slide__img" src="images/works-past.jpg" alt="worksスライド画像一覧"></div>
+                  <p class="slide__title">制作実績（非公開）</p>
+                  <p class="slide__text">ご面談時にご紹介できればと思います。</p>
+                </div>
+              </div>
+            </div>
+            <div class="swiper-controller">
+              <div class="swiper-button-prev mouse-stalker-link"></div>
+              <div class="swiper-button-next mouse-stalker-link"></div>
+            </div>
+            <div class="swiper-fraction">
+              <span class="swiper-fraction-current">
+                <span class="swiper-fraction-num"></span>
+              </span>
+              <span class="swiper-fraction-total"></span>
+            </div>
+          </div>
+          <!-- /swiper-main -->
+          
+          <!-- swiper-thumb -->
+          <div class="swiper swiper-thumb">
+            <div class="swiper-wrapper">
+              <div class="swiper-slide">
+                <div class="thumb mouse-stalker-link">
+                  <div class="thumb__img-wrapper">
+                    <img class="thumb__img" src="images/works-image1.jpg" alt="worksサムネイル画像一覧">
+                  </div>
+                </div>
+              </div>
+              <div class="swiper-slide">
+                <div class="thumb mouse-stalker-link">
+                  <div class="thumb__img-wrapper">
+                    <img class="thumb__img" src="images/works-image2.jpg" alt="worksサムネイル画像一覧">
+                  </div>
+                </div>
+              </div>
+              <div class="swiper-slide">
+                <div class="thumb mouse-stalker-link">
+                  <div class="thumb__img-wrapper">
+                    <img class="thumb__img" src="images/works-image3.jpg" alt="worksサムネイル画像一覧">
+                  </div>
+                </div>
+              </div>
+              <div class="swiper-slide">
+                <div class="thumb mouse-stalker-link">
+                  <div class="thumb__img-wrapper">
+                    <img class="thumb__img" src="images/works-image4.jpg" alt="worksサムネイル画像一覧">
+                  </div>
+                </div>
+              </div>
+              <div class="swiper-slide">
+                <div class="thumb mouse-stalker-link">
+                  <div class="thumb__img-wrapper">
+                    <img class="thumb__img" src="images/works-image5.jpg" alt="worksサムネイル画像一覧">
+                  </div>
+                </div>
+              </div>
+              <div class="swiper-slide">
+                <div class="thumb mouse-stalker-link">
+                  <div class="thumb__img-wrapper">
+                    <img class="thumb__img" src="images/works-past.jpg" alt="worksサムネイル画像一覧">
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="swiper-scrollbar mouse-stalker-link"></div>
+          </div>
+          <!-- /swiper-thumb -->
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- /works -->
+  
+  <!-- page-top -->
+  <div class="page-top">
+    <a href="#top">
+      <i class="page-top-icon fa-solid fa-chevron-up"></i>
+    </a>
+  </div>
+  <!-- /page-top -->
+</main>
